eb85648b16b1ca82138b5c5102023f1ae6a918a1
NarjesTaghlet <narjes.taghlet@insat.ucar.tn>
2025-03-07 13:48:00 +0100

    Fixed site error with proper DB setup

--- my-drupal-site/my-drupal-site/web/sites/default/settings.php
+++ my-drupal-site/my-drupal-site/web/sites/default/settings.php
@@ -22,4 +22,4 @@ $databases['default']['default'] = array (
   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
-$settings['hash_salt'] = 'VjX5HP3HG55-_efNTYIRaL08f5E51CPiN2IGY5WNWCsN6idlDjI9A5xcEsq6zqCYhRb4F2i-vg';
+$settings['hash_salt'] = 'l3smq0FQjkiR_dy_RNgcemffh30i8aXvaGyTeTxCF4KRF5Z0bl6Fw5VyVobxnXNjSqPE6RLMEg';
